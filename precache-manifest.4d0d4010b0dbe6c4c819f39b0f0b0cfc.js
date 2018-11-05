c4b45b06a3d8d89b54f44eb7819059c6121abc13
mjatta <30508772+mjatta@users.noreply.github.com>
2018-11-05 18:08:59 -0500

    PWA- from Dist from Server

---
+++ precache-manifest.4d0d4010b0dbe6c4c819f39b0f0b0cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "1ff5a10802136e9220e7",
+    "url": "/js/app.b84262df.js"
+  },
+  {
+    "revision": "cb4f507346bccc782144a16d48f34e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1ff5a10802136e9220e7",
+    "url": "/css/app.61b0428f.css"
+  }
+];
